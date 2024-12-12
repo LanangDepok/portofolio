f4390fce113021834876027baae1bd5d184a17fa
Bagas Rizkiyanto <bagasrizkiyanto2@gmail.com>
2024-12-13 02:41:22 +0700

    memperbaiki efek hover pada skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -23,115 +23,115 @@ const Skills = () => {
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-y-20 lg:gap-x-16">
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <FaHtml5 className="text-[#F16529] size-20" />
+          <FaHtml5 className="text-[#F16529] size-20 hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">HTML</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <FaCss3 className="text-[#2965F1] size-20 mx-auto" />
+          <FaCss3 className="text-[#2965F1] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">CSS</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <RiTailwindCssFill className="text-[#38B2AC] size-20 mx-auto" />
+          <RiTailwindCssFill className="text-[#38B2AC] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">Tailwind CSS</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <FaBootstrap className="text-[#6610f2] size-20 mx-auto" />
+          <FaBootstrap className="text-[#6610f2] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">Bootstrap</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <IoLogoJavascript className="text-[#F7DF1E] size-20 mx-auto" />
+          <IoLogoJavascript className="text-[#F7DF1E] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">JavaScript</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <SiTypescript className="text-[#007ACC] size-20 mx-auto" />
+          <SiTypescript className="text-[#007ACC] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">TypeScript</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <FaReact className="text-[#61DAFB] size-20 mx-auto" />
+          <FaReact className="text-[#61DAFB] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">React JS</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <SiNextdotjs className="text-[#FFFFFF] size-20 mx-auto" />
+          <SiNextdotjs className="text-[#FFFFFF] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">Next JS</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <RiPhpLine className="text-[#8993BE] size-20 mx-auto" />
+          <RiPhpLine className="text-[#8993BE] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">PHP</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <FaLaravel className="text-[#FF2D20] size-20 mx-auto" />
+          <FaLaravel className="text-[#FF2D20] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">Laravel</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <DiCodeigniter className="text-[#DD4814] size-20 mx-auto" />
+          <DiCodeigniter className="text-[#DD4814] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">CodeIgniter</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <GrMysql className="text-[#00758F] size-20 mx-auto" />
+          <GrMysql className="text-[#00758F] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">MySQL</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <BiLogoPostgresql className="text-[#336791] size-20 mx-auto" />
+          <BiLogoPostgresql className="text-[#336791] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">PostgreSQL</p>
         </div>
         <div
-          className="flex flex-col items-center transition duration-300 hover:scale-150"
+          className="flex flex-col items-center transition duration-300"
           data-aos="zoom-in-up"
           data-aos-duration="1500"
         >
-          <SiSqlite className="text-[#003B57] size-20 mx-auto" />
+          <SiSqlite className="text-[#003B57] size-20 mx-auto hover:scale-150 hover:-translate-y-5" />
           <p className="text-white">SQLite</p>
         </div>
       </div>
