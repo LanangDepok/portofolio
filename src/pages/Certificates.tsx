4e9311b41e03b4129b11eec982166e78b1d507b1
Bagas Rizkiyanto <bagasrizkiyanto2@gmail.com>
2024-12-22 14:52:52 +0700

    mengubah grid col pada sertifikat

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -20,7 +20,7 @@ const Certificates = () => {
           {locale === "en" ? "Certificates" : "Sertifikat"}
         </p>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-16">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
         <div>
           <ImageWithSkeleton src={LSP_informatika} alt="LSP Informatika" />
         </div>
